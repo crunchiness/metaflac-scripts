26c26772edd1f4f496a55a987e4812dea87c2c24
Ingvaras Merkys <ingvaras.merkys@westernunion.com>
2017-12-06 23:13:27 +0200

    improved sort_tags.sh to sort ALL THE TAGS

--- sort_tags.sh
+++ sort_tags.sh
@@ -10,34 +10,19 @@ fi
 IFS=$'\n' # split only on newline
 for x in `find ${DIRECTORY} -type f -name *.flac`
 do
-    ALBUM=`metaflac --show-tag=ALBUM "${x}"`
-    ARTIST=`metaflac --show-tag=ARTIST "${x}"`
-    DATE=`metaflac --show-tag=DATE "${x}"`
-    DISCNUMBER=`metaflac --show-tag=DISCNUMBER "${x}"`
-    DISCTOTAL=`metaflac --show-tag=DISCTOTAL "${x}"`
-    GENRE=`metaflac --show-tag=GENRE "${x}"`
-    TITLE=`metaflac --show-tag=TITLE "${x}"`
-    TRACKNUMBER=`metaflac --show-tag=TRACKNUMBER "${x}"`
-    TRACKTOTAL=`metaflac --show-tag=TRACKTOTAL "${x}"`
+    TAGS=`metaflac --export-tags-to=- "${x}"`
+    SORTED_TAGS=()
 
-    metaflac --remove-tag=ALBUM "${x}"
-    metaflac --remove-tag=ARTIST "${x}"
-    metaflac --remove-tag=DATE "${x}"
-    metaflac --remove-tag=DISCNUMBER "${x}"
-    metaflac --remove-tag=DISCTOTAL "${x}"
-    metaflac --remove-tag=GENRE "${x}"
-    metaflac --remove-tag=TITLE "${x}"
-    metaflac --remove-tag=TRACKNUMBER "${x}"
-    metaflac --remove-tag=TRACKTOTAL "${x}"
+    while read -r TAG; do
+        [[ "${TAG}" =~ ^([a-zA-Z ]+)\=.*$ ]] &&
+        SORTED_TAGS+=("${TAG}")
+        TAG_NAME="${BASH_REMATCH[1]}"
+        metaflac --remove-tag="${TAG_NAME}" "${x}"
+    done <<< "$TAGS"
 
-    metaflac --set-tag="${ALBUM}" "${x}"
-    metaflac --set-tag="${ARTIST}" "${x}"
-    metaflac --set-tag="${DATE}" "${x}"
-    metaflac --set-tag="${DISCNUMBER}" "${x}"
-    metaflac --set-tag="${DISCTOTAL}" "${x}"
-    metaflac --set-tag="${GENRE}" "${x}"
-    metaflac --set-tag="${TITLE}" "${x}"
-    metaflac --set-tag="${TRACKNUMBER}" "${x}"
-    metaflac --set-tag="${TRACKTOTAL}" "${x}"
-
+    SORTED_TAGS=($(sort <<< "${SORTED_TAGS[*]}"))
+    for SORTED_TAG in "${SORTED_TAGS[@]}"
+    do
+        metaflac --set-tag="${SORTED_TAG}" "${x}"
+    done
 done
