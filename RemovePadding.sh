ec03f976948abe0ae3750f4f71e63b9ff88467a0
Ingvaras Merkys <ingvaras.merkys@westernunion.com>
2017-12-06 23:54:03 +0200

    RemovePadding.sh now works on Mac

--- RemovePadding.sh
+++ RemovePadding.sh
@@ -1,2 +1,15 @@
-#!/bin/bash
-find "$1" -type f -name '*.flac' -printf "Removing padding for: %f\n" -exec metaflac --remove --block-type=PADDING --dont-use-padding {} \;
+#!/usr/bin/env bash
+
+DIRECTORY=${1%/}
+
+if [ ! -d "$DIRECTORY" ]; then
+    echo "${DIRECTORY} is not a directory"
+    exit 1
+fi
+
+IFS=$'\n' # split only on newline
+for x in `find ${DIRECTORY} -type f -name *.flac`
+do
+    printf "Removing padding for: %s\n" $(basename ${x})
+    metaflac --remove --block-type=PADDING --dont-use-padding "${x}"
+done
