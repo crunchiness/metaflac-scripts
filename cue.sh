d0eb6ad1c2a57d14fa537a8694791f6d1a5f39ae
Ingvaras Merkys <ingvaras@gmail.com>
2017-12-28 11:30:02 +0200

    cue.sh notes

--- cue.sh
+++ cue.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#sudo apt install shntool
+#sudo apt install cuetools
+
 shnsplit -o flac -f file.cue file.flac
+cuetag file.cue split-*.flac
 
 # TODO: split and write tags from the cue and flac
