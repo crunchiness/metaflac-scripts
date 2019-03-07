134c43b48b3fc95905774bd135f5ea274d3e62b1
Ingvaras Merkys <ingvaras@gmail.com>
2019-03-07 12:08:59 +0200

    Removed files replaced by change_tag.py

--- change_artist.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-ARTIST=${1}
-FILE=${2}
-
-metaflac --remove-tag=ARTIST "${FILE}"
-metaflac --set-tag=ARTIST="${ARTIST}" "${FILE}"
-metaflac --show-tag=ARTIST "${FILE}"
--- change_title.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-TITLE=${1}
-FILE=${2}
-
-metaflac --remove-tag=TITLE "${FILE}"
-metaflac --set-tag=TITLE="${TITLE}" "${FILE}"
-metaflac --show-tag=TITLE "${FILE}"
